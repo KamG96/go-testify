0dde836aa8bf10d4a85e862521b90a41aa891627
Kamil Garifov <kamilrok@yandex.ru>
2024-08-30 16:24:38 +0300

    second iteration

--- precode.go
+++ precode.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"net/http"
-	"net/http/httptest"
 	"strconv"
 	"strings"
-	"testing"
 )
 
 var cafeList = map[string][]string{
@@ -46,47 +42,4 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(answer))
-}
-
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	status := responseRecorder.Code
-	require.Equal(t, http.StatusOK, status)
-
-	body := responseRecorder.Body.String()
-	list := strings.Split(body, ",")
-
-	assert.Len(t, list, totalCount)
-}
-
-func TestMainHandlerWhenOk(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	status := responseRecorder.Code
-	assert.Equal(t, http.StatusOK, status)
-
-}
-
-func TestMainHandlerWhenCityNotFound(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?city=kazan", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	status := responseRecorder.Code
-	require.Equal(t, http.StatusBadRequest, status)
-
-	expected := `wrong city value`
-	assert.Equal(t, expected, responseRecorder.Body.String())
 }
---
+++ precode_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	status := responseRecorder.Code
+	require.Equal(t, http.StatusOK, status)
+
+	body := responseRecorder.Body.String()
+	list := strings.Split(body, ",")
+
+	assert.Len(t, list, totalCount)
+}
+
+func TestMainHandlerWhenOk(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	status := responseRecorder.Code
+	require.Equal(t, http.StatusOK, status)
+
+	body := responseRecorder.Body.String()
+	assert.NotEmpty(t, body)
+}
+
+func TestMainHandlerWhenCityNotFound(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=kazan", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	status := responseRecorder.Code
+	require.Equal(t, http.StatusBadRequest, status)
+
+	expected := `wrong city value`
+	assert.Equal(t, expected, responseRecorder.Body.String())
+}
